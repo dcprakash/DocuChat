eb1f611ef57e47376b623a181854de37e1a6756c
DarshanChinvar <chinvarpd@gmail.com>
2024-06-16 19:14:31 -0700

    modified chroma

--- src/modules/ConversationRetriever.py
+++ src/modules/ConversationRetriever.py
@@ -8,14 +8,14 @@ import streamlit as st
 from src.utils.openai_models import configure_endpoints
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+# from langchain_community.vectorstores import Chroma
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain, ConversationalRetrievalChain
 
 import bs4
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-# from langchain_chroma import Chroma
+from langchain_chroma import Chroma
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
