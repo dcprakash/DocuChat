ab182d3b620b8201d153ab53912cbc7c0fb12335
DarshanChinvar <chinvarpd@gmail.com>
2024-06-16 18:59:19 -0700

    modified vectorstores

--- src/modules/ConversationRetriever.py
+++ src/modules/ConversationRetriever.py
@@ -15,7 +15,7 @@ from langchain.chains import create_retrieval_chain, ConversationalRetrievalChai
 import bs4
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_chroma import Chroma # export HNSWLIB_NO_NATIVE=1  if installaion fails
+# from langchain_chroma import Chroma
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
